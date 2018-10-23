da93db0bc085ff0c1023eafdd34f6a8eeae9659f
Guy Harwood <guy@milkchip.com>
2018-10-23 09:40:28 +0100

    increment version

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-  res.status(200).send('Welcome to the restful API v1.0.0')
+  res.status(200).send('Welcome to the restful API v1.0.1')
 })
 
 const server = app.listen(3000, function () {
